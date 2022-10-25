8bf0b13d2d57e5b4b18f46103bed05fe5743c1b3
SpringLoach <1007435098@qq.com>
2022-10-25 20:58:53 +0800

    包管理

--- src/app/index.js
+++ src/app/index.js
@@ -2,10 +2,21 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const errorHandler = require("./error-handle"); // 添加
 const useRoutes = require("../router");
+const cors = require("koa2-cors");
 
 const app = new Koa();
 
 app.useRoutes = useRoutes;
+
+app.use(
+  cors({
+    origin: "*", // 允许来自指定域名请求
+    maxAge: 5, // 本次预检请求的有效期，单位为秒。
+    methods: ["GET", "POST"], // 所允许的HTTP请求方法
+    alloweHeaders: ["Conten-Type"], // 服务器支持的所有头信息字段
+    credentials: true, // 是否允许发送Cookie
+  })
+);
 
 app.use(bodyParser()); // 用于解析JSON
 app.useRoutes();
